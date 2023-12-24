938534b9ffc944b6d2cb4e4475761a1060b5fe3a
Богдан Савченко <91850335+SSovyshka@users.noreply.github.com>
2023-12-24 22:20:26 +0200

    Delete sim_frontend directory

--- sim_frontend/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-## 0.0.1
-**Migration Guide**
-- Update theme files
-- Ng-core add PrimeNG 16
-- Ng-core add Angular 16
--- sim_frontend/src/app/models/message.return.pojo.model.ts
+++
@@ -1,6 +0,0 @@
-export class MessageReturnPojoModel {
-    constructor(
-        public id?: number,
-        public message?: string
-    ) {}
-}
